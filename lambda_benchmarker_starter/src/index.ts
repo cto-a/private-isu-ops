1615490f9411d7168bcc235d43d2753a03803b8c
harukitosa <13haruki28@gmail.com>
2023-06-19 01:34:17 +0900

    feat taskの起動

--- lambda_benchmarker_starter/src/index.ts
+++ lambda_benchmarker_starter/src/index.ts
@@ -4,12 +4,11 @@ import { setTimeout } from "timers/promises"
 
 const sqs = new SQS({ region: "ap-northeast-1" })
 
-
 /**
  * spread sheetから参加者の情報を取得する
- * 
- * @param url 
- * @returns 
+ *
+ * @param url
+ * @returns
  */
 const fetchRunningDataFromGoogleSpreadSheet = async (url: string) => {
     const response = await fetch(url)
@@ -19,17 +18,24 @@ const fetchRunningDataFromGoogleSpreadSheet = async (url: string) => {
 
 export const handler = async (event) => {
     console.log(event)
+    if (event.teamId === undefined) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify("Error: teamId is not defined"),
+        }
+    }
+    const { teamId } = event
     const sheetsApiUrl = process.env.GOOGLE_SHEETS_API
-    const queueUrl = process.env.SQS_QUEUE_URL || "https://sqs.ap-northeast-1.amazonaws.com/254374927794/benchmark_queue"
+    const queueUrl =
+        process.env.SQS_QUEUE_URL || "https://sqs.ap-northeast-1.amazonaws.com/254374927794/benchmark_queue"
     console.log("url: " + sheetsApiUrl)
     try {
-	const data = await fetchRunningDataFromGoogleSpreadSheet(sheetsApiUrl)
-	console.log(JSON.stringify(data))
+        const data = await fetchRunningDataFromGoogleSpreadSheet(sheetsApiUrl)
+        console.log(JSON.stringify(data))
     } catch (e) {
-	console.error(e)
-	return;
+        console.error(e)
+        return
     }
-    const teamId = event.teamId;
     const targetIp = "http://54.249.115.183"
     console.log("teamId: " + teamId)
     const params = {
@@ -39,26 +45,50 @@ export const handler = async (event) => {
                 DataType: "String",
                 StringValue: targetIp,
             },
-	    teamId: {
-		DataType: "Number",
-		StringValue: teamId,
-	    }
+            teamId: {
+                DataType: "Number",
+                StringValue: teamId,
+            },
         },
         MessageBody: "TESTです。",
         QueueUrl: queueUrl,
     }
+
+    // SQSへ、実行したTeamIdとIPアドレスを送信する
     try {
-	console.log("send message")
-	const command = new SendMessageCommand(params)
+        console.log("send message")
+        const command = new SendMessageCommand(params)
         await sqs.send(command)
-    } catch(e) {
-	console.error("error debug")
-	console.error(e)
-	return {
-		statusCode: 500,
-		body: JSON.stringify(`Error :${e}`),
-	}
+    } catch (e) {
+        console.error("error debug")
+        console.error(e)
+        return {
+            statusCode: 500,
+            body: JSON.stringify(`Error :${e}`),
+        }
     }
+
+    // CloudTaskの起動
+    const ecs = new AWS.ECS()
+    const taskResult = await ecs
+        .runTask({
+            cluster: "benchmarker-ecs-cluster",
+            taskDefinition: "benchmark-ecs-service",
+            launchType: "FARGATE",
+            count: 1,
+            //     networkConfiguration: {
+            // awsvpcConfiguration: {
+            //     subnets: [ECS_SUBNET],
+            //     assignPublicIp: "ENABLED",
+            // },
+            //     },
+        })
+        .promise()
+
+    if (taskResult.failures != null) {
+        console.error(taskResult.failures)
+    }
+
     const response = {
         statusCode: 200,
         body: JSON.stringify("Hello from Lambda!"),
