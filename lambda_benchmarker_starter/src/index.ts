8a6af7b6628923bc6726d6560fd92dc725228324
harukitosa <13haruki28@gmail.com>
2023-06-26 20:11:26 +0900

    fix error throw

--- lambda_benchmarker_starter/src/index.ts
+++ lambda_benchmarker_starter/src/index.ts
@@ -23,11 +23,11 @@ const fetchRunningDataFromGoogleSpreadSheet = async (url: string) => {
 
 /**
  * ECSのタスクを起動する
- * 
- * @returns 
+ *
+ * @returns
  */
 const runTask = async () => {
-	// TODO: ここ環境変数経由にして
+    // TODO: ここ環境変数経由にして
     const input: RunTaskCommandInput = {
         cluster: "benchmarker-ecs-cluster",
         taskDefinition: "benchmarker-ecs-task-definition:18",
@@ -63,7 +63,7 @@ export const handler = async (event) => {
         process.env.SQS_QUEUE_URL || "https://sqs.ap-northeast-1.amazonaws.com/254374927794/benchmark_queue"
     console.log("url: " + sheetsApiUrl)
 
-    let sheetsData;
+    let sheetsData
     try {
         sheetsData = await fetchRunningDataFromGoogleSpreadSheet(sheetsApiUrl)
         console.log(JSON.stringify(sheetsData))
@@ -72,26 +72,31 @@ export const handler = async (event) => {
         return
     }
     const teamList: {
-        teamId: string;
-        teamName: string;
-        teamIp: string;
-      }[] = []      
+        teamId: string
+        teamName: string
+        teamIp: string
+    }[] = []
     // sheetsDataのデータからチームID、チーム名、チームIPをteamList配列に入れ直す
-    sheetsData.sheets[0].data[0].rowData.forEach( data => {
-        const teamData ={
-          teamId: data.values[0]?.formattedValue,
-          teamName: data.values[1]?.formattedValue,
-          teamIp: data.values[2]?.formattedValue
-        };
+    sheetsData.sheets[0].data[0].rowData.forEach((data) => {
+        const teamData = {
+            teamId: data.values[0]?.formattedValue,
+            teamName: data.values[1]?.formattedValue,
+            teamIp: data.values[2]?.formattedValue,
+        }
         teamList.push(teamData)
-    }
-    )
+    })
     // 接続元IPのチーム名を抽出する
-    const targetTeam = teamList.filter(team => team.teamIp == sourceIp);
+    const targetTeam = teamList.filter((team) => team.teamIp == sourceIp)
     // 複数あった場合は最初のものを利用する
-    const targetIp = targetTeam[0]?.teamIp || sourceIp
-    const teamId = targetTeam[0]?.teamId || 9999
-    const teamName = targetTeam[0]?.teamName || 'no data'
+    const targetIp = sourceIp
+    const teamId = targetTeam[0]?.teamId
+    const teamName = targetTeam[0]?.teamName
+    if (targetIp === undefined || teamId === undefined || teamName === undefined) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify(`Error : targetIp:${targetIp} teamId:${teamId} teamName:${teamName} is undefined`),
+        }
+    }
     console.log("targetIp: " + targetIp)
     console.log("teamId: " + teamId)
     console.log("teamId: " + teamName)
@@ -131,7 +136,7 @@ export const handler = async (event) => {
 
     // CloudTaskの起動
     try {
-	console.log("run task")
+        console.log("run task")
         const data = await runTask()
         console.log("run task result : " + JSON.stringify(data))
     } catch (e) {
