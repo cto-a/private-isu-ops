9310bb1f8c79b324bb1e05b4507cc973da8c9376
kesompochy <kesompochy@gmail.com>
2024-07-30 15:07:33 +0900

    sqsにアクセスできないとダメ

--- benchmarker/terraform/ecs.tf
+++ benchmarker/terraform/ecs.tf
@@ -424,6 +424,26 @@ resource "aws_iam_policy" "ssm_policy" {
 EOF
 }
 
+resource "aws_iam_policy" "ecs_sqs_access_policy" {
+  name        = "ecs-sqs-access-policy"
+  description = "Allow ECS tasks to access SQS"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes"
+        ]
+        Resource = "arn:aws:sqs:ap-northeast-1:009160051284:benchmark_queue"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_ssm_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = aws_iam_policy.ssm_policy.arn
@@ -432,6 +452,11 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_ssm_policy" {
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = aws_iam_policy.ecs_task_execution_role_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_sqs_policy_attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = aws_iam_policy.ecs_sqs_access_policy.arn
 }
 
 resource "aws_cloudwatch_log_group" "ecs_log" {
